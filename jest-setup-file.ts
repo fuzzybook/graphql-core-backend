0cfbcd44e2743d9b93267b6144296347a3552f96
Fuzzybook <fabio@prada.ch>
2022-04-06 18:29:51 +0200

    setup jest

---
+++ jest-setup-file.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+
+process.env.POSTGRES_HOST = '127.0.0.1';
+process.env.POSTGRES_USER = 'fantaskipper';
+process.env.POSTGRES_PASSWORD = 'fantaskipper';
+process.env.POSTGRES_DB = 'fantaskipper';
